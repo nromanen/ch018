19cf4a72d2da8bd6807d032de6ddd37a881994c7
yuremboo <yurik.my@gmail.com>
2013-12-15 20:01:14 +0200

    Added BooksInUse, Bookcase entity

--- src/main/java/com/ch018/library/DAO/BookDAOImpl.java
+++ src/main/java/com/ch018/library/DAO/BookDAOImpl.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.ch018.library.entity.Book;
 import com.ch018.library.entity.Person;
 
-@Component
+@Repository
 public class BookDAOImpl implements BookDAO {
 
 	@Autowired
--- src/main/java/com/ch018/library/util/HibernateUtil.java
+++ src/main/java/com/ch018/library/util/HibernateUtil.java
@@ -1,4 +1,4 @@
-package com.ch018.library.DAO.util;
+package com.ch018.library.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
