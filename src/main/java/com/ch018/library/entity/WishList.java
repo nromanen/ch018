f5a24e0834b12509d2e6c387911d7a028bdc1eac
sasha <alex9523@yandex.ru>
2013-12-17 14:11:48 +0200

    wishlist+

---
+++ src/main/java/com/ch018/library/entity/WishList.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ch018.library.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="wishlist")
+public class WishList {
+    private int id;
+    private int booksID;
+    private int personId;
+
+    public WishList() {
+    }
+        
+    public void setId(int id){
+        this.id=id;
+    }
+    
+    public void setBooksId(int booksId){
+        this.booksID=booksId;
+    }
+    
+    public void setPersonId(int pId){
+       this.personId=pId; 
+    }
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId(){
+        return this.id;
+    }
+    
+    @Column(name="Books_id")
+    public int getBooksId(){
+        return this.booksID;
+    }
+    
+    @Column(name="person_id")
+    public int getPersonId(){
+        return this.personId;
+    }
+}
