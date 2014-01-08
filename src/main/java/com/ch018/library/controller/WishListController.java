1b6b00a590dc95b9af493360c720a15fa75924ce
unknown <okryvortc@chclass13.training.local>
2014-01-08 18:29:51 +0200

    1111

--- src/main/java/com/ch018/library/controller/WishListController.java
+++ src/main/java/com/ch018/library/controller/WishListController.java
@@ -77,7 +77,7 @@ public class WishListController {
       int personId = person.getId();
       
       if(wish.bookExistInWishList(bookId, personId)){
-             return "redirect:/authorizedUser"; 
+             return "redirect:/"; 
       }else{
              WishList newWish = new WishList();
              newWish.setPerson(person);
