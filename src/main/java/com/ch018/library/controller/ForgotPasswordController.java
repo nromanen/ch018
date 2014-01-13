9b60c34472ae5d5b9f298c8e3918061741993e68
yuremboo <yurik.my@gmail.com>
2014-01-13 18:35:11 +0200

    valid reset pass

--- src/main/java/com/ch018/library/controller/ForgotPasswordController.java
+++ src/main/java/com/ch018/library/controller/ForgotPasswordController.java
@@ -1,28 +1,30 @@
 package com.ch018.library.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ch018.library.entity.Person;
-import com.ch018.library.form.Password;
 import com.ch018.library.form.ResetPassword;
 import com.ch018.library.service.PersonService;
+import com.ch018.library.validator.ResetPwdValidation;
 
 @Controller
 public class ForgotPasswordController {
 
 	@Autowired
 	private PersonService personService;
+	
+	@Autowired
+	private ResetPwdValidation resetPwdValidation;
 
 	@Secured("ROLE_ANONYMOUS")
 	@RequestMapping(value = "/remind", method = RequestMethod.GET)
@@ -60,7 +62,7 @@ public class ForgotPasswordController {
 	
 	@Secured("ROLE_ANONYMOUS")
 	@RequestMapping(value = "/remind/pass", method = RequestMethod.POST)
-	public String passwordRestore(@ModelAttribute ResetPassword resetPassword) {
+	public String passwordRestore(@ModelAttribute @Valid ResetPassword resetPassword) {
 		Person person = personService.getByKey(resetPassword.getKey());
 		personService.restorePassword(person, resetPassword);
 		return "redirect:/";
