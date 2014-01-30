ce0143e0227a43c2344dc9b5b8a31d696e80da57
yuremboo <yurik.my@gmail.com>
2014-01-30 23:32:07 +0200

    images fix

--- Library_common/library.sql
+++ Library_common/library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Янв 26 2014 г., 16:08
+-- Время создания: Янв 30 2014 г., 22:31
 -- Версия сервера: 5.6.14
 -- Версия PHP: 5.5.6
 
@@ -42,6 +42,7 @@ CREATE TABLE IF NOT EXISTS `books` (
   `title` varchar(200) NOT NULL,
   `year_public` int(11) DEFAULT '0',
   `gid` int(11) NOT NULL,
+  `numberOfEvaluations` int(11) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `FK_k00r52dx96mgbrvv8i05saupq` (`gid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
@@ -50,19 +51,19 @@ CREATE TABLE IF NOT EXISTS `books` (
 -- Дамп данных таблицы `books`
 --
 
-INSERT INTO `books` (`id`, `authors`, `available`, `bookcase`, `count`, `description`, `image`, `pages`, `publication`, `rating`, `shelf`, `term`, `title`, `year_public`, `gid`) VALUES
-(2, 'Antonio Goncalves', 5, 1, 5, 'Beginning Java EE 7 is one of the first tutorials written with definitive expertise on the Java EE 7 platform.', 'https://www.apress.com/media/catalog/product/cache/9/small_image/9df78eab33525d08d6e5fb8d27136e95/A/9/A9781430246268-small_4.png', 608, 'Apress', 0, 1, 14, 'Beginning Java EE 7', 2013, 2),
-(3, 'Fain Y.', 5, 2, 5, 'As one of the most popular software languages for building Web applications, Java is often the first programming language developers learn.', 'http://i31.fastpic.ru/big/2011/1224/c0/ad98f9c71227aa37a7e2fb2c6d4539c0.jpg', 470, 'WILEY', 0, 4, 14, ' Java Programming 24-Hour Trainer', 2011, 2),
-(4, 'Craig Walls', 5, 2, 5, 'Spring in Action, Third Edition, is for all Java developers', 'http://i26.fastpic.ru/big/2011/0712/42/5d3223e783805d76392181b12c60e642.jpg', 401, 'Manning', 0, 2, 14, 'Spring in action', 2011, 2),
-(5, 'K. Sierra, B. Bates', 5, 2, 5, 'With hundreds of practice questions and hands-on exercises, SCJP Sun Certified Programmer for Java 6 Study Guide covers what you need to know--and shows you how to prepare--for this challenging exam.', 'https://d3hgnfpzeohxco.cloudfront.net/images/bau/97800715/9780071591065/0/0/plain/scjp-sun-certified-programmer-for-java-6-study-guide-exam-310-065.jpg', 890, 'MC Graw Hill', 0, 3, 14, 'SCJP', 2008, 2),
-(6, 'M. Deinum & K. Serneels', 5, 1, 5, 'Pro Spring MVC is an in-depth guide to Spring MVC, a modern web framework build on top of the Spring Framework.', 'http://www.apress.com/media/catalog/product/cache/9/image/9df78eab33525d08d6e5fb8d27136e95/A/9/A9781430241553-3d_5.png', 590, 'Apress', 0, 1, 14, 'Pro Spring MVC: with Web Flow', 2011, 2),
-(14, 'Joanna Martine Woolfolk', 5, 10, 5, 'Everyone''s favorite astrology book, having sold over 500,000 copies, is now even easier to use with an interactive CD-ROM', 'http://d23a3s5l1qjyz.cloudfront.net/wp-content/uploads/2011/05/astrology-book.jpg', 461, 'Taylor Trade Publishing', 0, 1, 14, 'The Only Astrology Book You''ll Ever Need', 2006, 6),
-(15, 'Roger L Tokheim', 2, 6, 2, 'Designed to be used as an introductory text for students new to the electronics field', 'http://i.ebayimg.com/t/Digital-Electronics-Roger-L-Tokheim-1993-Hardcover/00/$T2eC16V,!yEE9s5jFKheBRbsjOE%2BUw~~_35.JPG', 392, 'World', 0, 1, 14, 'Digital Electronics', 1988, 4),
-(16, 'J.R.R. Tolkien', 5, 12, 5, 'In a hole in the ground there lived a hobbit...', 'http://d202m5krfqbpi5.cloudfront.net/books/1372847500l/5907.jpg', 365, 'Houghton Mifflin', 0, 1, 14, 'The Hobbit (Middle-Earth Universe)', 2002, 10),
-(17, 'Jared Diamond', 8, 14, 8, 'Life isn''t fair--here''s why: Since 1500, Europeans have, for better & worse, called the tune that the world has danced to.', 'http://d202m5krfqbpi5.cloudfront.net/books/1363934734l/1842.jpg', 494, 'W.W. Norton & Company', 0, 2, 14, 'Guns, Germs, and Steel: The Fates of Human Societies', 2005, 1),
-(18, 'Clifford A. Pickover', 3, 15, 3, 'Math’s infinite mysteries and beauty unfold in this follow-up to the best-selling The Science Book', 'http://ecx.images-amazon.com/images/I/41PsllMMlCL._SX258_PJlook-inside-v2,TopRight,1,0_SH20_BO1,204,203,200_.jpg', 985, 'The Science Book', 0, 1, 14, 'The Math Book', 2008, 3),
-(19, 'Friedrich Nietzsche, Walter Kaufmann', 5, 20, 5, 'Thus Spoke Zarathustra: A Book for All and None Thus is a philosophical novel by German philosopher Friedrich Nietzsche', 'http://d202m5krfqbpi5.cloudfront.net/books/1349449118l/51893.jpg', 327, 'Penguin Books', 0, 2, 14, 'Thus Spoke Zarathustra', 1978, 7),
-(20, 'Jon Ronson', 6, 11, 6, 'The Psychopath Test is a fascinating journey through the minds of madness.', 'http://d202m5krfqbpi5.cloudfront.net/books/1307825196l/9378733.jpg', 288, 'Riverhead Hardcover', 0, 2, 14, 'The Psychopath Test: A Journey Through the Madness Industry', 2011, 9);
+INSERT INTO `books` (`id`, `authors`, `available`, `bookcase`, `count`, `description`, `image`, `pages`, `publication`, `rating`, `shelf`, `term`, `title`, `year_public`, `gid`, `numberOfEvaluations`) VALUES
+(2, 'Antonio Goncalves', 5, 1, 5, 'Beginning Java EE 7 is one of the first tutorials written with definitive expertise on the Java EE 7 platform.', '/resources/img/books/javaee7.png', 608, 'Apress', 0, 1, 14, 'Beginning Java EE 7', 2013, 2, 0),
+(3, 'Fain Y.', 5, 2, 5, 'As one of the most popular software languages for building Web applications, Java is often the first programming language developers learn.', '/resources/img/books/java24.jpg', 470, 'WILEY', 0, 4, 14, 'Java Programming 24-Hour Trainer', 2011, 2, 0),
+(4, 'Craig Walls', 5, 2, 5, 'Spring in Action, Third Edition, is for all Java developers', '/resources/img/books/springina.jpg', 401, 'Manning', 0, 2, 14, 'Spring in action', 2011, 2, 0),
+(5, 'K. Sierra, B. Bates', 5, 2, 5, 'With hundreds of practice questions and hands-on exercises, SCJP Sun Certified Programmer for Java 6 Study Guide covers what you need to know--and shows you how to prepare--for this challenging exam.', '/resources/img/books/scjp.jpg', 890, 'MC Graw Hill', 0, 3, 14, 'SCJP', 2008, 2, 0),
+(6, 'M. Deinum & K. Serneels', 5, 1, 5, 'Pro Spring MVC is an in-depth guide to Spring MVC, a modern web framework build on top of the Spring Framework.', '/resources/img/books/webflow.png', 590, 'Apress', 0, 1, 14, 'Pro Spring MVC: with Web Flow', 2011, 2, 0),
+(14, 'Joanna Martine Woolfolk', 5, 10, 5, 'Everyone''s favorite astrology book, having sold over 500,000 copies, is now even easier to use with an interactive CD-ROM', '/resources/img/books/astro.jpg', 461, 'Taylor Trade Publishing', 0, 1, 14, 'The Only Astrology Book You''ll Ever Need', 2006, 6, 0),
+(15, 'Roger L Tokheim', 2, 6, 2, 'Designed to be used as an introductory text for students new to the electronics field', '/resources/img/books/digital.JPG', 392, 'World', 0, 1, 14, 'Digital Electronics2', 1988, 4, 0),
+(16, 'J.R.R. Tolkien', 5, 12, 5, 'In a hole in the ground there lived a hobbit...', '/resources/img/books/hobbit.jpg', 365, 'Houghton Mifflin', 0, 1, 14, 'The Hobbit (Middle-Earth Universe)', 2002, 10, 0),
+(17, 'Jared Diamond', 8, 14, 8, 'Life isn''t fair--here''s why: Since 1500, Europeans have, for better & worse, called the tune that the world has danced to.', '/resources/img/books/guns.jpg', 494, 'W.W. Norton & Company', 0, 2, 14, 'Guns, Germs, and Steel: The Fates of Human Societies', 2005, 5, 0),
+(18, 'Clifford A. Pickover', 3, 15, 3, 'Math’s infinite mysteries and beauty unfold in this follow-up to the best-selling The Science Book', '/resources/img/books/math.jpg', 985, 'The Science Book', 0, 1, 14, 'The Math Book', 2008, 3, 0),
+(19, 'Friedrich Nietzsche, Walter Kaufmann', 5, 20, 5, 'Thus Spoke Zarathustra: A Book for All and None Thus is a philosophical novel by German philosopher Friedrich Nietzsche', '/resources/img/books/nitzshe.jpg', 327, 'Penguin Books', 0, 2, 14, 'Thus Spoke Zarathustra', 1978, 7, 0),
+(20, 'Jon Ronson', 6, 11, 6, 'The Psychopath Test is a fascinating journey through the minds of madness.', '/resources/img/books/psycho.jpg', 288, 'Riverhead Hardcover', 0, 2, 14, 'The Psychopath Test: A Journey Through the Madness Industry', 2011, 9, 0);
 
 -- --------------------------------------------------------
 
@@ -78,6 +79,7 @@ CREATE TABLE IF NOT EXISTS `booksinuse` (
   `term` int(11) DEFAULT NULL,
   `Books_id` int(11) DEFAULT NULL,
   `Person_id` int(11) DEFAULT NULL,
+  `mark` int(11) DEFAULT '0',
   PRIMARY KEY (`buid`),
   KEY `FK_2nkoqh845najnu1vfv20lkp6p` (`Books_id`),
   KEY `FK_rece9y8w4qeg63ksgcrecwn54` (`Person_id`)
@@ -210,7 +212,7 @@ CREATE TABLE IF NOT EXISTS `person` (
 --
 
 INSERT INTO `person` (`id`, `cellphone`, `confirmed`, `e_mail`, `emailConfirmed`, `failedOrders`, `multibookAllowed`, `name`, `password`, `rating`, `role`, `sms`, `surname`, `timely_returns`, `untimely_returns`, `verificationkey`) VALUES
-(1, '(099) 274-5624', 1, 'yurik.my@gmail.com', 1, 0, 9, 'Yurik', '$2a$10$Gp.aP6XfF5mZRyzrL5FNTuiw2wyoTOjZFsrLPl9Tg8/qaICgK7qRK', 0.95, 'ROLE_LIBRARIAN', 1, 'Mikhaletskiy', 19, 1, '168be49084d0181543480732768535f521da7af3'),
+(1, '(099) 274-5624', 1, 'yurik.my@gmail.com', 1, 0, 9, 'Yurik', '$2a$10$Gp.aP6XfF5mZRyzrL5FNTuiw2wyoTOjZFsrLPl9Tg8/qaICgK7qRK', 0.957, 'ROLE_LIBRARIAN', 1, 'Mikhaletskiy', 22, 1, '168be49084d0181543480732768535f521da7af3'),
 (3, '(097) 108-4550', 1, 'yurik_my@mail.ru', 1, 0, 10, 'Yurik', '$2a$10$yS2y6mjHpHf5/TaHPDcYYuPN6Bv6vjdIt2Bmx.R7rpuE1oDaQBAme', 0, 'ROLE_USER', 0, 'Mykh', 7, 0, '310dcc42a9e9662dac0282e77251a8e18986c947'),
 (4, '(050) 555-5555', 1, 'yurik.my@yandex.ru', 1, 0, 8, 'John', '$2a$10$Fzcrwk4TCHf9JqCQRDGu7.jfpMbwzB0SnDFkC95i1u9BT0NnpA1Ym', 0.818, 'ROLE_USER', 0, 'Terry', 9, 2, 'b1d2661a53328efcd97ae4acf1fa5dbb153e97c1'),
 (5, '(050) 222-2222', 1, 'test@test.tst', 1, 0, 10, 'Joe', '$2a$10$F1V1TRrhiDFeFnqaSj822OHKA5lm41f7u21ffIAUEcD1vQ44xSz8m', 1, 'ROLE_USER', 0, 'Cole', 1, 0, 'cf926b2cb8bb29d2b189a5d482016aabfa8742af'),
@@ -231,7 +233,15 @@ CREATE TABLE IF NOT EXISTS `wishlist` (
   PRIMARY KEY (`id`),
   KEY `FK_qc2mmx7r8ebvjkf4x67qv02hf` (`Books_id`),
   KEY `FK_8fo1hianyjf9k9pcdh1jlcxdq` (`Person_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `wishlist`
+--
+
+INSERT INTO `wishlist` (`id`, `Books_id`, `Person_id`) VALUES
+(1, 3, 1),
+(2, 5, 1);
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -247,8 +257,8 @@ ALTER TABLE `books`
 -- Ограничения внешнего ключа таблицы `booksinuse`
 --
 ALTER TABLE `booksinuse`
-  ADD CONSTRAINT `FK_rece9y8w4qeg63ksgcrecwn54` FOREIGN KEY (`Person_id`) REFERENCES `person` (`id`),
-  ADD CONSTRAINT `FK_2nkoqh845najnu1vfv20lkp6p` FOREIGN KEY (`Books_id`) REFERENCES `books` (`id`);
+  ADD CONSTRAINT `FK_2nkoqh845najnu1vfv20lkp6p` FOREIGN KEY (`Books_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `FK_rece9y8w4qeg63ksgcrecwn54` FOREIGN KEY (`Person_id`) REFERENCES `person` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `localization`
